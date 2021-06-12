95ab194f655b0c9b0d6f306dd0814680078c0edf
Twinf0x <bastian.7741@gmail.com>
2021-06-12 22:30:51 +0200

    Don't rotate player sprites towards the camera

--- Assets/Scripts/Visuals/PlayerSprite.cs
+++ Assets/Scripts/Visuals/PlayerSprite.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 public class PlayerSprite : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-    private Camera targetCamera;
-
-    private void Awake()
-    {
-        targetCamera = Camera.main;
-    }
-
-    private void Update()
-    {
-        Vector3 targetPosition = targetCamera.transform.position;
-        targetPosition.y = transform.position.y;
-        transform.LookAt(targetPosition, Vector3.up);
-    }
 
     public void SetSprite(GridDirection direction)
     {
