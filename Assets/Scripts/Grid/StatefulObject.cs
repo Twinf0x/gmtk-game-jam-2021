d6d376c4f43a82bd55bf8985e180e0c3e67affd9
Twinf0x <bastian.7741@gmail.com>
2021-06-13 17:12:31 +0200

    Add deadly stateful objects

--- Assets/Scripts/Grid/StatefulObject.cs
+++ Assets/Scripts/Grid/StatefulObject.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class StatefulObject : MonoBehaviour
 {
     #region Inspector
+    [SerializeField] private bool isDeadly = false;
     [SerializeField] private GameObject blockingState;
     [SerializeField] private GameObject nonBlockingState;
     [Header("Audio")]
     [SerializeField] private AudioSource blockingSFX;
     [SerializeField] private AudioSource nonBlockingSFX;
+    [Header("References - Scene")]
+    [SerializeField] private LevelController levelController;
     #endregion
 
     public void SetBlocking()
@@ -17,6 +20,15 @@ public class StatefulObject : MonoBehaviour
         blockingState.SetActive(true);
         nonBlockingState.SetActive(false);
         blockingSFX.Play();
+
+        if (isDeadly)
+        {
+            var movable = Grid.GetMovableObjectFromWorldPosition(transform.position);
+            if (movable != null)
+            {
+                levelController.LoseLevel();
+            }
+        }
     }
 
     public void SetNonBlocking()
